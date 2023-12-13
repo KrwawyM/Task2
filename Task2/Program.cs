63741d8c32f511fcfc625fccf715103f43be8948
wikto <wikto@KOMPWIKI>
2023-12-13 13:47:44 +0100

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = 'A';
+            char second = 'B';
+            char third = 'C';
+
+            Console.WriteLine(third);
+            Console.WriteLine(second);
+            Console.WriteLine(first);
+
+
+        } 
+    } 
+}
+
